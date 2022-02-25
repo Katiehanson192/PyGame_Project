4560c2697f6c760330a248c3e102527a853c550d
Katie Hanon <katie_hanson@baylor.edu>
2022-02-24 18:27:50 -0600

    installed VE + gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+
 import pygame
 
 class AlienInvasion:
@@ -7,19 +8,20 @@ class AlienInvasion:
     ##initialize game, and create game assests and behavior.
     def __init__(self):
         pygame.init() #why don't we need a "self." argument??
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1200, 800))  #represents entire game window
+
         pygame.display.set_caption("Alien Invasion")
 
     def run_game():
         #starting main loop for the game!
         while True:
             #looking for keyboard/mouse clicks from user
-            for event in pygame.event.get():
+            for event in pygame.event.get():  #returns list of events that have taken place since last time the function was called
                 if event.type ==pygame.QUIT:
                     sys.exit()
             
             #make most recently drawn screen visible
-            pygame.display.flip()
+            pygame.display.flip() #How it game elements move/dissapear
 
 if __name__=='__main__':
     #making a game instance and running the game
