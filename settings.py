121acfde04780db78b3426eab87e5c31f782b9ef
Katie Hanon <katie_hanson@baylor.edu>
2022-02-26 18:50:41 -0600

    Bullet settings

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
         #Ship settings
         self.ship_speed = 1.5  #determines how far to move the ship on each pass through the loop
                                 #need this b/c rect.x and rect.y only accept integers
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
