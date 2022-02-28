e6dd93ef2c59c5c04ad2b52b2220bd8eb2c48c8b
Katie Hanon <katie_hanson@baylor.edu>
2022-02-28 10:36:06 -0600

    checking if alien hits the edge

--- alien.py
+++ alien.py
@@ -23,7 +23,16 @@ class Alien(Sprite):
         #store the alien's exact horizontal position
         self.x = float(self.rect.x) #tracks horizontal speed 
 
+    def check_edges(self):
+        #return True if alien is at edge of screen
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <=0: #if alien position to the right is less than or equal to the right screen position value 
+                                                                        #or the the left position value is 0, give answer True--> tell aliens to move right
+            return True
+
     def update(self):
-        #move alien to the right
+        #move alien to the right or left
+        self.x +=(self.settings.alien_speed *
+                        self.settings.fleet_direction) #alien_speed * fleet_direction b/c determines if fleet moves left or right
         self.x +=self.settings.alien_speed #update alien's position each time by moving it the right by amt stored in alien_speed
         self.rect.x = self.x #updates value of rect.x, which is the alien's position (self.x can store decimals and rect.x can't)
