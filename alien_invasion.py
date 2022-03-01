cac96450467392152eb55a1b7b31bfdb2a3aaec3
Katie Hanon <katie_hanson@baylor.edu>
2022-02-28 19:52:53 -0600

    deactivating the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -72,7 +72,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         #start a new game when the player clicks play
-        if self.play_button.rect.collidepoint(mouse_pos): #if mouse click collides w/ play button, make game active
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos) #only will start if play button is clicked AND game is currently inactive
+        if button_clicked and not self.stats.game_active: #if mouse click collides w/ play button, make game active
             #reset game stats
             self.stats.reset_stats() #reset game stats, gives player 3 more ships
             self.stats.game_active = True
