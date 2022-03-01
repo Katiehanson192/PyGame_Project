a38957b6637650a8d32a532b4ed8b759080db096
Katie Hanon <katie_hanson@baylor.edu>
2022-03-01 12:21:36 -0600

    Rounded score and comma

--- scoreboard.py
+++ scoreboard.py
@@ -18,7 +18,8 @@ class Scoreboard:
 
     def prep_score(self):
         #turn scoreboard into rendered image
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1) #-1 for round tells python to round to the nearest 10
+        score_str="{:,}".format(rounded_score) #adds commas to large numbers
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
 
         #display score at top right of screen
